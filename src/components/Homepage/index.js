e83a90f038b3d9d68edf84efff867326ec899d01
Jessica Anocibar <jessicayaws@yahoo.com>
2020-11-29 19:11:13 -0700

    added resume button

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -25,12 +26,16 @@ export default function ContainedButtons() {
         <h4>I will complete the Full Stack Coding Bootcamp from University of Denver in just a few short weeks. I have come a long way from not knowing much about web development to building an enormous amount of knowledge within a 3 month time period. These new skills include Javascript, CSS, HTML, React, Node.js, MongoDB, Mongoose, Sequelize, and many other languages. Collaborating, communicating, and being task-oriented are key values I bring to any project. Learning multiple programs and languages within such a short amount of time as prepared me for the ever-changing field of technology. </h4> 
 
     
-      <Button variant="contained" color="primary" href="#contained-buttons" onClick={() => window.open('https://www.linkedin.com/in/jessica-anocibar-b9b129127/')}>
-        LinkedIn
+      <Button style={{width:170,backgroundColor:'#99C794', marginTop:20,}} variant="contained" color="primary" href="#contained-buttons" onClick={() => window.open('https://www.linkedin.com/in/jessica-anocibar-b9b129127/')}>
+       LinkedIn
       </Button>
-      <Button variant="contained" color="primary" href="#contained-buttons" onClick={() => window.open('https://github.com/jessicaano92')}>
+      <Button style={{width:170,backgroundColor:'#99C794', marginTop:20,}} variant="contained" color="primary" href="#contained-buttons" onClick={() => window.open('https://github.com/jessicaano92')}>
         GitHub
       </Button>
+      <Button style={{width:170,backgroundColor:'#99C794', marginTop:20,}} variant="contained" color="primary" href="#contained-buttons" onClick={() => window.open('https://docs.google.com/document/d/1F6aipLgaDzNLwIJZgoIHJHdhiikNOkfGXKCeDfNn1CE/edit?usp=sharing')}>
+        Resume
+      </Button>
+    
     </div>
     </>
   );
