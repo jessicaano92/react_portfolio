42984f72365f5bef1afb18d68b1c1c97ffc60d41
Jessica Anocibar <jessicayaws@yahoo.com>
2021-01-14 22:08:24 -0700

    styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,22 @@ import "./Home.css"
 
 
 
+
 function Home () {
     return (
         <>
 
         <div>
-            <h1 className="name" style={{letterSpacing: "0.181818em"}}>Jessica Anocibar</h1>
+            <h1 className="name" style={{letterSpacing: "0.181818em", color: 'whitesmoke'}}>Jessica Anocibar</h1>
         </div>
         <div className="topNav">
-            <a href="/Home" style={{letterSpacing: "0.181818em"}}>Home </a>
-            <a href="/Biography" style={{letterSpacing: "0.181818em"}}>Biography </a>
-            <a href="/Projects" style={{letterSpacing: "0.181818em"}}>Projects </a>
-            <a href="/Contact" style={{letterSpacing: "0.181818em"}}>Contact</a>
+            <a href="/Home" style={{letterSpacing: "0.181818em", color: 'whitesmoke'}}>Home </a>
+            <a href="/Biography" style={{letterSpacing: "0.181818em", color: 'whitesmoke'}}>Biography </a>
+            <a href="/Projects" style={{letterSpacing: "0.181818em", color: 'whitesmoke'}}>Projects </a>
+            <a href="/Contact" style={{letterSpacing: "0.181818em", color: 'whitesmoke'}}>Contact</a>
         </div>
 
+       
       
     
             
